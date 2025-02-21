6e9314fd0fe8a8a5a3aaee521c98d1e8a867948f
Adam <adamdoxaev5@gmail.com>
2025-02-21 11:55:27 +0300

    reading file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,57 @@
 use std::env;
+use std::error::Error;
+use std::fs;
 
+// cargo run -- test poem.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // args[0] это имя програмы
-    let query = &args[1];
-    let file_path = &args[2];
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     println!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = match Config::build(&args) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Problem parsing arguments: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        eprintln!("Error reading file: {}", e);
+        std::process::exit(1);
+    }
+}
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        // args[0] это имя програмы
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {query, file_path})
+    }
 }
