28a08a802eedea87ea7bb08a0a95c11b25172201
Adam <adamdoxaev5@gmail.com>
2025-02-21 10:43:34 +0300

    accepts arguments via command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Test commit");
+    let args: Vec<String> = env::args().collect();
+
+    // args[0] это имя програмы
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
